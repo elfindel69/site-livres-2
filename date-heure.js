8fb7f2b9265a6ff60b4be26789d0647a564137b0
Valentin Michel <elfindel69@gmail.com>
2019-01-28 21:16:35 +0100

    modifs horloge

--- date-heure.js
+++ date-heure.js
@@ -1,19 +1,43 @@
-function compZero(nombre) {
-    return nombre < 10 ? '0' + nombre : nombre;
-}
-
-function date_heure() {
-    const infos = new Date();
-
-    //Heure
-    document.getElementById('date_heure').innerHTML = 'Il est ' + compZero(infos.getHours()) + ' heures ' + compZero(infos.getMinutes()) + ', ' + compZero(infos.getSeconds());
-
-    //Date
-    const mois = new Array('janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'août', 'septembre', 'octobre', 'novembre', 'décembre');
-    const jours = new Array('dimanche', 'lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi');
-    document.getElementById('date_heure').innerHTML += ' et nous sommes le ' + jours[infos.getDay()] + ' ' + infos.getDate() + ' ' + mois[infos.getMonth()] + ' ' + infos.getFullYear() + '.';
-}
-
-window.onload = function() {
-    setInterval(date_heure(), 1000); //Actualisation de l'heure
+// constante
+const HEURE = 1000 * 60 * 60;
+// Données d'affichage et de traitement
+var data = [
+    { 'id': 'papeete', 'utc': -10 }, // -10 UTC
+    { 'id': 'martinique', 'utc': -4 }, // -04 UTC
+    { 'id': 'saint-pierre', 'utc': -3 }, // -03 UTC
+    { 'id': 'brest', 'utc': +1 }, // +01 UTC
+    { 'id': 'reunion', 'utc': +4 }, // +04 UTC
+    { 'id': 'noumea', 'utc': +11 }, // +11 UTC
+    { 'id': 'londres', 'utc': 0 }
+];
+// format d'affichage pour les navigateurs récents ;)
+// https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Objets_globaux/Date/toLocaleString
+var optionsDate = {
+    //  hour12: false,
+    weekday: 'long',
+    //  year:     'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit'
 };
+// Différence entre le fuseau horaire UTC, et celui de l'heure locale.
+var offsetDate = new Date().getTimezoneOffset() / 60;
+// Fonction d'affichage
+function afficheHeure() {
+    let i,
+        nb = data.length,
+        dTemp,
+        date = new Date();
+    // boucle sur les infos à afficher
+    for (i = 0; i < nb; i += 1) {
+        dTemp = new Date();
+        dTemp.setTime(date.getTime() + ((data[i].utc + offsetDate) * HEURE));
+        document.getElementById(data[i].id).innerHTML = dTemp.toLocaleString('fr-FR', optionsDate);
+    }
+}
+// lance affichage toutes les prochaines secondes
+setInterval(afficheHeure, 1000);
+// affichage immédiat
+afficheHeure();
