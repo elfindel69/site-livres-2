dfe0002d671451f9486aadb7bb5b5747ae71e870
Valentin Michel <elfindel69@gmail.com>
2019-01-28 23:11:55 +0100

    modif horloges

--- date-heure.js
+++ date-heure.js
@@ -1,41 +1,46 @@
-// constante
-const HEURE = 1000 * 60 * 60;
 // Données d'affichage et de traitement
 var data = [
-    { 'id': 'papeete', 'utc': -10 }, // -10 UTC
-    { 'id': 'martinique', 'utc': -4 }, // -04 UTC
-    { 'id': 'saint-pierre', 'utc': -3 }, // -03 UTC
-    { 'id': 'brest', 'utc': +1 }, // +01 UTC
-    { 'id': 'reunion', 'utc': +4 }, // +04 UTC
-    { 'id': 'noumea', 'utc': +11 }, // +11 UTC
-    { 'id': 'londres', 'utc': 0 }
+    { 'id': 'TFR', 'utc': 'America/Toronto' },
+    { 'id': 'AFR', 'utc': 'Africa/Nairobi' },
+    { 'id': 'CAC', 'utc': 'America/Guyana' },
+    { 'id': 'NAM', 'utc': 'America/New_York' },
+    { 'id': 'SAM', 'utc': 'America/Montevideo' },
+    { 'id': 'EUA', 'utc': 'Europe/Moscow' },
+    { 'id': 'EUR', 'utc': 'Europe/Brussels' },
+    { 'id': 'MES', 'utc': 'Africa/Cairo' },
+    { 'id': 'SEA', 'utc': 'Asia/Jakarta' },
+    { 'id': 'SWA', 'utc': 'Asia/Tehran' },
+    { 'id': 'OCN', 'utc': 'Pacific/Auckland' },
+    { 'id': 'UTC', 'utc': 'UTC' }
 ];
 // format d'affichage pour les navigateurs récents ;)
 // https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Objets_globaux/Date/toLocaleString
 var optionsDate = {
-    //  hour12: false,
+    hour12: false,
     weekday: 'long',
-    //  year:     'numeric',
     month: 'long',
     day: 'numeric',
+    year: 'numeric',
     hour: '2-digit',
     minute: '2-digit',
-    second: '2-digit'
+    second: '2-digit',
+    timeZoneName: 'short'
 };
-// Différence entre le fuseau horaire UTC, et celui de l'heure locale.
-var offsetDate = new Date().getTimezoneOffset() / 60;
+
 // Fonction d'affichage
 function afficheHeure() {
     let i,
         nb = data.length,
-        dTemp,
         date = new Date();
     // boucle sur les infos à afficher
     for (i = 0; i < nb; i += 1) {
         dTemp = new Date();
-        dTemp.setTime(date.getTime() + ((data[i].utc + offsetDate) * HEURE));
-        document.getElementById(data[i].id).innerHTML = dTemp.toLocaleString('fr-FR', optionsDate);
+        optionsDate.timeZone = data[i].utc;
+        document.getElementById(data[i].id).innerHTML = date.toLocaleString('fr-FR', optionsDate);
     }
+
+    optionsDate.timeZone = "UTC";
+    document.getElementById("londres").innerHTML = date.toLocaleString("fr-FR", optionsDate);
 }
 // lance affichage toutes les prochaines secondes
 setInterval(afficheHeure, 1000);
