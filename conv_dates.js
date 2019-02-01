aa3ae61bdaf03f300269e030612921c438c55007
Valentin Michel <elfindel69@gmail.com>
2019-02-01 14:25:06 +0100

    modifs ts

--- conv_dates.js
+++ conv_dates.js
@@ -1,23 +1,23 @@
-"use strict";
 /**
  * module d'horloge
  * affiche l'heure au format UTC, GTC et TimeCode
  * @author elfindel69
  * @version 0.0.200
  */
-exports.__esModule = true;
-var utils_1 = require("./utils");
-var UTCDate_1 = require("./UTCDate");
-// Fonction d'affichage
-function afficheHeure() {
-    var lDate = new Date();
-    document.getElementById("UTC").innerHTML = lDate.toLocaleString("fr-FR", UTCDate_1.optionsDate);
-    var lDateGTC = UTCDate_1.UTCDateToGTCDate(lDate);
-    document.getElementById("GTC").innerHTML = lDateGTC.getGTCDateString();
-    document.getElementById("DTC").innerHTML = utils_1.GTCDateToTC(lDateGTC.mYear, lDateGTC.mDays);
-    document.getElementById("TMC").innerHTML = utils_1.GTCDateToDC(lDateGTC.mYear, lDateGTC.mDays, lDateGTC.mTimestamp);
-}
-// lance affichage toutes les prochaines secondes
-setInterval(afficheHeure, 1000);
-// affichage immédiat
-afficheHeure();
+define(["require", "exports", "./utils", "./UTCDate"], function (require, exports, utils_1, UTCDate_1) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    // Fonction d'affichage
+    function afficheHeure() {
+        var lDate = new Date();
+        document.getElementById("UTC").innerHTML = lDate.toLocaleString("fr-FR", UTCDate_1.optionsDate);
+        var lDateGTC = UTCDate_1.UTCDateToGTCDate(lDate);
+        document.getElementById("GTC").innerHTML = lDateGTC.getGTCDateString();
+        document.getElementById("DTC").innerHTML = utils_1.GTCDateToTC(lDateGTC.mYear, lDateGTC.mDays);
+        document.getElementById("TMC").innerHTML = utils_1.GTCDateToDC(lDateGTC.mYear, lDateGTC.mDays, lDateGTC.mTimestamp);
+    }
+    // lance affichage toutes les prochaines secondes
+    setInterval(afficheHeure, 1000);
+    // affichage immédiat
+    afficheHeure();
+});
--- form_dates.js
+++ form_dates.js
@@ -1,44 +1,44 @@
-"use strict";
 /**
  * module de conversions de dates
  * @author elfindel69
  * @version 0.0.200
  */
-exports.__esModule = true;
-var UTCDate_1 = require("./UTCDate");
-var utils_1 = require("./utils");
-var myForm = document.getElementById('myForm');
-myForm.addEventListener('reset', function () {
-    var inputs = document.querySelectorAll('input[type=text]');
-    var inputsLength = inputs.length;
-    for (var i = 0; i < inputsLength; i += 1) {
-        inputs[i].nodeValue = '';
+define(["require", "exports", "./UTCDate", "./utils"], function (require, exports, UTCDate_1, utils_1) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    var myForm = document.getElementById('myForm');
+    myForm.addEventListener('reset', function () {
+        var inputs = document.querySelectorAll('input[type=text]');
+        var inputsLength = inputs.length;
+        for (var i = 0; i < inputsLength; i += 1) {
+            inputs[i].nodeValue = '';
+        }
+    }, false);
+    var elem = document.getElementById('convert');
+    function afficheDates(UTCdate, GTCDate) {
+        document.getElementById("UTCTime").innerHTML = UTCdate.toLocaleString("fr-FR", UTCDate_1.optionsDate);
+        document.getElementById("GTCTime").innerHTML = GTCDate.getGTCDateString();
+        document.getElementById("DTCTime").innerHTML = utils_1.GTCDateToTC(GTCDate.mYear, GTCDate.mDays);
+        document.getElementById("TMCTime").innerHTML = utils_1.GTCDateToDC(GTCDate.mYear, GTCDate.mDays, GTCDate.mTimestamp);
     }
-}, false);
-var elem = document.getElementById('convert');
-function afficheDates(UTCdate, GTCDate) {
-    document.getElementById("UTCTime").innerHTML = UTCdate.toLocaleString("fr-FR", UTCDate_1.optionsDate);
-    document.getElementById("GTCTime").innerHTML = GTCDate.getGTCDateString();
-    document.getElementById("DTCTime").innerHTML = utils_1.GTCDateToTC(GTCDate.mYear, GTCDate.mDays);
-    document.getElementById("TMCTime").innerHTML = utils_1.GTCDateToDC(GTCDate.mYear, GTCDate.mDays, GTCDate.mTimestamp);
-}
-function convert() {
-    var lGTCTime = document.getElementById('dateGTC').nodeValue;
-    var lUTCTime = document.getElementById('dateUTC').nodeValue;
-    var lUTCDate;
-    var lGTCDate;
-    if (lUTCTime !== "") {
-        lUTCDate = new Date(lUTCTime);
+    function convert() {
+        var lGTCTime = document.getElementById('dateGTC').nodeValue;
+        var lUTCTime = document.getElementById('dateUTC').nodeValue;
+        var lUTCDate;
+        var lGTCDate;
+        if (lUTCTime !== "") {
+            lUTCDate = new Date(lUTCTime);
+        }
+        if (lGTCTime === "") {
+            lGTCDate = UTCDate_1.UTCDateToGTCDate(lUTCDate);
+        }
+        afficheDates(lUTCDate, lGTCDate);
     }
-    if (lGTCTime === "") {
-        lGTCDate = UTCDate_1.UTCDateToGTCDate(lUTCDate);
+    elem.onclick = convert;
+    function afficheDate() {
+        var lUTCTime = new Date();
+        var lGTCTime = UTCDate_1.UTCDateToGTCDate(lUTCTime);
+        afficheDates(lUTCTime, lGTCTime);
     }
-    afficheDates(lUTCDate, lGTCDate);
-}
-elem.onclick = convert;
-function afficheDate() {
-    var lUTCTime = new Date();
-    var lGTCTime = UTCDate_1.UTCDateToGTCDate(lUTCTime);
-    afficheDates(lUTCTime, lGTCTime);
-}
-afficheDate();
+    afficheDate();
+});
