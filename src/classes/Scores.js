f89a5f292b5ecd5049b60d523856bade4e07fa48
Valentin Michel <elfindel69@gmail.com>
2019-09-24 11:49:27 +0200

    modifs classes

---
+++ src/classes/Scores.js
@@ -0,0 +1,11 @@
+/**
+ * Module de calcul de score
+ * @author elfindel69
+ * @version 0.2.0
+ */
+export class Scores {
+    constructor(score1, score2) {
+        this.score1 = score1;
+        this.score2 = score2;
+    }
+}
---
+++ src/classes/countries.js
@@ -0,0 +1,14 @@
+/**
+ * Classe pays
+ * @author elfindel69
+ * @version 0.2.0
+ */
+
+export class Country {
+
+    constructor(name, oldPts, scores) {
+        this.name = name;
+        this.oldPts = oldPts;
+        this.scores = scores;
+    }
+}
